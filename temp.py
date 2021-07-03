9ee9829b0938d2f2b46ac2ed15b1c1c640b4a424
ehine02 <56832762+ehine02@users.noreply.github.com>
2021-07-03 15:27:10 +0100

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,17 +1,17 @@
+import datetime
+import pandas as pd
 from statsbombpy import sb
 
 
 def main():
-    comps = sb.competitions()
-    #print(comps.head(5))
-
-    comp = 37
-    season = 42
-    matches = sb.matches(competition_id=comp, season_id=season)
-    #print(matches.head(5))
     match = 2275038
-    lineup = sb.lineups(match_id=match)['West Ham United LFC']
-    #print(lineup.head(10))
-
     events = sb.events(match_id=match)
-    print(events.head(20))
+
+    events['timestamp'] = pd.to_datetime(events['timestamp'])
+    chances = events[events.shot.isna()==False].timestamp
+    frame = []
+    for end in chances.to_list():
+        start = end - datetime.timedelta(seconds=30)
+        mask = (events['timestamp'] > start) & (events['timestamp'] <= end)
+        frame.append(events.loc[mask])
+    return frame
