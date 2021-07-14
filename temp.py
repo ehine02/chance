c4d85a50bde183ea874aeb54703907a29dec99d6
ehine02 <ehine02@dcs.bbk.ac.uk>
2021-07-14 14:26:03 +0100

    Time slicing

--- temp.py
+++ temp.py
@@ -1,14 +1,17 @@
 import datetime
 import pandas as pd
+import numpy as np
 from statsbombpy import sb
 
 
 def main():
-    match = 2275038
+    match = 18245
     events = sb.events(match_id=match)
     events.to_csv(str(match) + '.csv')
 
-    events = events[events['period']==2]
+    events = events[events['period']==1]
+    events = events.join(pd.json_normalize(events['pass']), rsuffix='_pass')
+    events = events.join(pd.json_normalize(events['shot']), rsuffix='_shot')
     events = events.set_index(pd.DatetimeIndex(events['timestamp']))
     events = events.sort_index()
     return events
