029ec0e32c6f033b91d10c20070f8a48b4c584f8
ehine02 <ehine02@dcs.bbk.ac.uk>
2021-08-11 11:36:01 +0100

    Time slicing

--- text.py
+++ text.py
@@ -15,21 +15,40 @@ from tensorflow.python.keras.layers import Conv1D, MaxPooling1D
 
 #numpy.random.seed(7)
 
+def euclidean_distance(start, end):
+    return np.sqrt(np.power(end[0] - start[0], 2) + np.power(end[1] - start[1], 2))
+
+
+def list_if_not_nan(x):
+    if x is np.nan:
+        return np.nan
+    else:
+        return ast.literal_eval(x)
+
+
+def split_location(x):
+    if x is not np.nan:
+        return round(x[0], 0), round(x[1], 0)
+    return np.nan, np.nan
+
 
 def load_events():
-    e = pd.read_csv('all_events.csv', nrows=50000)
+    e = pd.read_csv('all_events.csv', nrows=10000)
     e = e.loc[~e['shot_type'].isin(['Penalty'])]
     e = e.loc[~e['location'].isin([np.nan])]
-    e['location'] = e.location.apply(ast.literal_eval)
-    e['location_x'] = e.location.apply(lambda x: round(x[0], 0))
-    e['location_y'] = e.location.apply(lambda x: round(x[1], 0))
-    e['pass_end_x'] = e.pass_end_location.apply(lambda x: round(x[0], 0))
-    e['pass_end_y'] = e.pass_end_location.apply(lambda x: round(x[1], 0))
-    e['carry_end_x'] = e.carry_end_location.apply(lambda x: round(x[0], 0))
-    e['carry_end_y'] = e.carry_end_location.apply(lambda x: round(x[1], 0))
+    e['location'] = e.location.apply(list_if_not_nan)
+    e['pass_end_location'] = e.pass_end_location.apply(list_if_not_nan)
+    e['carry_end_location'] = e.carry_end_location.apply(list_if_not_nan)
+    e['location_x'], e['location_y'] = zip(*e.location.map(split_location))
+    e['pass_end_x'], e['pass_end_y'] = zip(*e.pass_end_location.map(split_location))
+    e['carry_end_x'], e['carry_end_y'] = zip(*e.carry_end_location.map(split_location))
+    e.loc[e.type == 'Carry', 'carry_length'] = \
+        np.sqrt((e.carry_end_x-e.location_x)**2 + (e.carry_end_y-e.location_y)**2)
+    e.loc[e.type == 'Carry', 'carry_speed'] = e.carry_length / e.duration
+    e.loc[e.type == 'Pass', 'pass_speed'] = e.pass_length / e.duration
     e['chance'] = ~e['shot_type'].isna()
     e.fillna(value=0.0, inplace=True)
-    e = e.drop(columns=['location'])
+    e = e.drop(columns=['location', 'pass_end_location', 'carry_end_location'])
     return e
 
 
@@ -47,18 +66,22 @@ def location_to_text(x, y):
     return 'boxedge'
 
 
-def pass_speed_to_text(duration, length):
+def pass_speed(duration, length):
     if duration in [np.nan, 0.0] or length in [np.nan, 0.0]:
         return ''
     speed = length / duration
     if speed < 10:
-        return 'slow'
+        return 'tap'
     if speed > 18:
-        return 'fast'
-    return 'medium'
+        return 'ping'
+    return 'safe'
 
 
-def progressive_action(start, end):
+def carry_length():
+    pass
+
+
+def carry_speed():
     pass
 
 
@@ -77,10 +100,14 @@ def main():
         chance = False
         events_list = []
         for _, row in events.iterrows():
-            events_list.append(location_to_text(row['location_x'], row['location_y']))
-            events_list.append(pass_speed_to_text(row['duration'], row['pass_length']))
-            events_list.append(row['type'])
+            event_type = str(row['type']).lower()
             chance = chance or row['chance']
+            events_list.append(location_to_text(row['location_x'], row['location_y']))
+            if event_type in ['shot', 'goal keeper']:
+                continue
+            events_list.append(pass_speed(row.duration, row.pass_length))
+
+            events_list.append(event_type)
         text = text.append({'text': ' '.join(events_list), 'chance': chance}, ignore_index=True)
     print('MAX EVENTS:', max_events)
     df = text.copy()
@@ -102,12 +129,13 @@ def classy(text):
     y = text.pop('chance').astype(int)
     t = keras.preprocessing.text.Tokenizer()
     t.fit_on_texts(text.text.tolist())
-    encoded = t.texts_to_matrix(text.text.tolist())
-    x_train, x_test, y_train, y_test = train_test_split(encoded, y, test_size=0.1, random_state=0)
+    x_train, x_test, y_train, y_test = train_test_split(text, y, test_size=0.1, random_state=0)
+    x_train_encoded = t.texts_to_matrix(x_train.text.tolist())
+    x_test_encoded = t.texts_to_matrix(x_test.text.tolist())
     # truncate and pad input sequences
     max_possession_events = 120
-    x_train = sequence.pad_sequences(x_train, maxlen=max_possession_events)
-    x_test = sequence.pad_sequences(x_test, maxlen=max_possession_events)
+    x_train_encoded = sequence.pad_sequences(x_train_encoded, maxlen=max_possession_events)
+    x_test_encoded = sequence.pad_sequences(x_test_encoded, maxlen=max_possession_events)
     # create the model
     embedding_vector_length = 32
     model = Sequential()
@@ -122,11 +150,11 @@ def classy(text):
                                                                          keras.metrics.FalsePositives(),
                                                                          keras.metrics.FalseNegatives()])
     print(model.summary())
-    model.fit(x_train, y_train, epochs=10, batch_size=64)
+    model.fit(x_train_encoded, y_train, epochs=10, batch_size=64)
     # Final evaluation of the model
-    scores = model.evaluate(x_test, y_test, verbose=0)
+    scores = model.evaluate(x_test_encoded, y_test, verbose=0)
     print("Accuracy: %.2f%%" % (scores[1] * 100))
-    y_pred = [round(i[0], 0) for i in model.predict(x_test)]
+    y_pred = [round(i[0], 0) for i in model.predict(x_test_encoded)]
     print(confusion_matrix(y_test, y_pred))
-    #'possession': {},
-    return pd.DataFrame({'actual': y_test, 'predicted': y_pred}), x_test
+
+    return pd.DataFrame({'seq': x_test.text, 'actual': y_test, 'predicted': y_pred})
