19c6eceabc44e8a0239471d83e1b1bb6e782fcbe
Ed <ehine02@dcs.bbk.ac.uk>
2021-09-03 17:17:38 +0100

    Beginning refactoring Including xG regression for numeric sequences

--- raw_ts.py
+++ raw_ts.py
@@ -36,8 +36,8 @@ def load_events():
         round(np.sqrt((120 - e.carry_end_x) ** 2 + (40 - e.carry_end_y) ** 2), 0)
     e.loc[e.to_goal_end != np.nan, 'progression_pct'] = round(100 * (e.to_goal_start - e.to_goal_end) /
                                                               e.to_goal_start, 0)
-    e['delta_y'] = e.location_y.diff()
-    e['delta_x'] = e.location_x.diff()
+    e['delta_y'] = e.location_y.diff().abs()
+    e['delta_x'] = e.location_x.diff().abs()
 
     e['chance'] = ~e['shot_type'].isna()
     xg = XgMap()
@@ -51,9 +51,9 @@ def load_events():
     return e
 
 
-def build_numeric_sequences():
+def build_numeric_sequences(target):
     # Parameters
-    dimensions = ['pass_speed', 'pass_length', 'carry_speed', 'carry_length']
+    dimensions = ['pass_speed', 'pass_length', 'carry_speed', 'carry_length', 'delta_y']
 
     e = load_events()
     e.type = e.type.str.lower()
@@ -69,10 +69,10 @@ def build_numeric_sequences():
     sequences = []
     target_chance = []
     target_xg = []
-    max_seq = 0
+    longest_seq = 0
     for ((match_id, possession), events) in g:
-        if len(events.index) > max_seq:
-            max_seq = len(events.index)
+        if len(events.index) > longest_seq:
+            longest_seq = len(events.index)
         events = events.set_index(events['index'])
         events = events.sort_index()
         events.fillna(value=0.0, inplace=True)
@@ -86,44 +86,61 @@ def build_numeric_sequences():
                                                 'chance': target_chance,
                                                 'xg': target_xg}))
     print(seq_df.chance.value_counts())
-    return seq_df, max_seq
+    return seq_df.seqs, seq_df[target], longest_seq
 
 
-def classy():
-    df, longest_seq = build_numeric_sequences()
+def pad_sequences(sequences, padding_shape, value):
+    # padded_shape is (longest, width)
+    padded = np.full((len(sequences), padding_shape[0], padding_shape[1]), fill_value=value)
+    for index, sequence in enumerate(sequences):
+        padded[index, 0:sequence.shape[0], :] = sequence
+    return padded
 
-    x = np.array(df.seqs.to_list())
-    y = np.array([[int(t)] for t in df.chance.to_list()])
-    print(x.shape)
-    print(y.shape)
 
-    seq_count = x.shape[0]
-    seq_width = len(x[0][0])
-
-    special_value = -10.0
-    x_pad = np.full((seq_count, longest_seq, seq_width), fill_value=special_value)
-    for s, x in enumerate(x):
-        seq_len = x.shape[0]
-        x_pad[s, 0:seq_len, :] = x
-
-    x_train, x_test, y_train, y_test = train_test_split(x_pad, y, test_size=0.2, random_state=0)
-    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.1, random_state=0)
-
+def classification_model(mask_value, input_shape):
     model = Sequential()
-    model.add(Masking(mask_value=special_value, input_shape=(longest_seq, seq_width)))
+    model.add(Masking(mask_value=mask_value, input_shape=input_shape))
     model.add(LSTM(64))
     model.add(Dropout(0.3))
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy',
-                  optimizer=WAME(learning_rate=0.0001),#keras.optimizers.Adam(learning_rate=0.0001),
-                  metrics=['accuracy',
-                           keras.metrics.Precision(),
-                           keras.metrics.Recall(),
-                           keras.metrics.FalsePositives(),
-                           keras.metrics.FalseNegatives()])
+                  optimizer=WAME(learning_rate=0.0001),
+                  metrics=['accuracy'])
     print(model.summary())
+    return model
 
-    h = model.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=200, batch_size=1024)
+
+def regression_model(mask_value, input_shape):
+    model = Sequential()
+    model.add(Masking(mask_value=mask_value, input_shape=input_shape))
+    model.add(LSTM(64))
+    model.add(Dropout(0.3))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='mse',
+                  optimizer=WAME(learning_rate=0.0001),
+                  metrics=['mean_squared_error'])
+    print(model.summary())
+    return model
+
+
+def classy():
+    sequences, targets, longest_sequence = build_numeric_sequences(target='chance')
+    padding_shape = (longest_sequence, len(sequences[0][0]))  # (longest, width)
+    padding_value = -10.0
+    sequences_padded = pad_sequences(sequences, padding_shape, padding_value)
+
+    x_train, x_test, y_train, y_test = train_test_split(sequences_padded, targets, test_size=0.2, random_state=0)
+    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.1, random_state=0)
+
+    model = classification_model(mask_value=padding_value,
+                                 input_shape=padding_shape)
+
+    h = model.fit(x_train,
+                  y_train,
+                  validation_data=(x_val, y_val),
+                  epochs=10,
+                  batch_size=1024)
+
     scores = model.evaluate(x_test, y_test, verbose=True)
     print("Accuracy: %.2f%%" % (scores[1] * 100))
     y_prob = [i[0] for i in model.predict(x_test)]
@@ -131,45 +148,31 @@ def classy():
     print(confusion_matrix(y_test, y_pred))
     print(classification_report(y_test, y_pred))
     plot_history(h, 'accuracy')
-    return scores, model, pd.DataFrame({'actual': [i[0] for i in y_test], 'predicted': y_pred, 'prob': y_prob})
+    return scores, model, pd.DataFrame({'actual': y_test, 'predicted': y_pred, 'prob': y_prob})
 
 
 def chancy():
-    df, longest_seq = build_numeric_sequences()
+    sequences, targets, longest_sequence = build_numeric_sequences(target='xg')
+    padding_shape = (longest_sequence, len(sequences[0][0]))  # (longest, width)
+    padding_value = -10.0
+    sequences_padded = pad_sequences(sequences, padding_shape, padding_value)
 
-    x = np.array(df.seqs.to_list())
-    y = np.array([[t] for t in df.xg.to_list()])
-    print(x.shape)
-    print(y.shape)
-
-    seq_count = x.shape[0]
-    seq_width = len(x[0][0])
-
-    special_value = -10.0
-    x_pad = np.full((seq_count, longest_seq, seq_width), fill_value=special_value)
-    for s, x in enumerate(x):
-        seq_len = x.shape[0]
-        x_pad[s, 0:seq_len, :] = x
-
-    x_train, x_test, y_train, y_test = train_test_split(x_pad, y, test_size=0.2, random_state=0)
+    x_train, x_test, y_train, y_test = train_test_split(sequences_padded, targets, test_size=0.2, random_state=0)
     x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.1, random_state=0)
 
-    model = Sequential()
-    model.add(Masking(mask_value=special_value, input_shape=(longest_seq, seq_width)))
-    model.add(LSTM(64))
-    model.add(Dropout(0.3))
-    model.add(Dense(1, activation='sigmoid'))
-    model.compile(loss='mse',
-                  optimizer=WAME(learning_rate=0.0001),#keras.optimizers.Adam(learning_rate=0.0001),
-                  metrics=['mean_squared_error'])
-    print(model.summary())
+    model = regression_model(mask_value=padding_value,
+                             input_shape=padding_shape)
 
-    h = model.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=200, batch_size=1024)
-    # Final evaluation of the model
+    h = model.fit(x_train,
+                  y_train,
+                  validation_data=(x_val, y_val),
+                  epochs=10,
+                  batch_size=2048)
+
     scores = model.evaluate(x_test, y_test, verbose=True)
     print("MSE: %.2f%%" % scores[1])
     y_prob = [i[0] for i in model.predict(x_test)]
 
     print('R2 Score:', r2_score(y_test, y_prob))
     plot_history(h, 'mean_squared_error')
-    return pd.DataFrame({'actual': [i[0] for i in y_test], 'prob': y_prob})
+    return pd.DataFrame({'actual': y_test, 'prob': y_prob})
