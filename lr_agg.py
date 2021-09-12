88f4b23fe4c754d604ce2bea4f0b062277e7d210
ehine02 <ehine02@dcs.bbk.ac.uk>
2021-09-12 14:44:29 +0100

    Further refactoring, introducing test run harness

--- lr_agg.py
+++ lr_agg.py
@@ -1,59 +1,13 @@
-import keras.optimizers
 import pandas as pd
 import numpy as np
-import ast
 
-from matplotlib import pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report, roc_auc_score
 
+from sb_interface import load_events
 from utils import perform_oversampling
-
-
-def list_if_not_nan(x):
-    if x is np.nan:
-        return np.nan
-    else:
-        return ast.literal_eval(x)
-
-
-def split_location(x):
-    if x is not np.nan:
-        return round(x[0], 0), round(x[1], 0)
-    return np.nan, np.nan
-
-
-def load_events():
-    e = pd.read_csv('all_events_orig_bak.csv', nrows=100000)
-    e = e.loc[~e['shot_type'].isin(['Penalty'])]
-    e = e.loc[~e['location'].isin([np.nan])]
-    e['location'] = e.location.apply(list_if_not_nan)
-    e['pass_end_location'] = e.pass_end_location.apply(list_if_not_nan)
-    e['carry_end_location'] = e.carry_end_location.apply(list_if_not_nan)
-    e['location_x'], e['location_y'] = zip(*e.location.map(split_location))
-    e['pass_end_x'], e['pass_end_y'] = zip(*e.pass_end_location.map(split_location))
-    e['carry_end_x'], e['carry_end_y'] = zip(*e.carry_end_location.map(split_location))
-    e.loc[e.type == 'Carry', 'carry_length'] = \
-        np.sqrt((e.carry_end_x - e.location_x) ** 2 + (e.carry_end_y - e.location_y) ** 2)
-    e.loc[e.type == 'Carry', 'carry_speed'] = e.carry_length / e.duration
-    e.loc[e.type == 'Pass', 'pass_speed'] = e.pass_length / e.duration
-    e.loc[e.location != np.nan, 'to_goal_start'] = \
-        round(np.sqrt((120 - e.location_x) ** 2 + (40 - e.location_y) ** 2), 0)
-    e.loc[e.type == 'Pass', 'to_goal_end'] = \
-        round(np.sqrt((120 - e.pass_end_x) ** 2 + (40 - e.pass_end_y) ** 2), 0)
-    e.loc[e.type == 'Carry', 'to_goal_end'] = \
-        round(np.sqrt((120 - e.carry_end_x) ** 2 + (40 - e.carry_end_y) ** 2), 0)
-    e.loc[e.to_goal_end != np.nan, 'progression_pct'] = round(100 * (e.to_goal_start - e.to_goal_end) /
-                                                              e.to_goal_start, 0)
-    e['delta_y'] = e.location_y.diff()
-    e['delta_x'] = e.location_x.diff()
-
-    e['chance'] = ~e['shot_type'].isna()
-    e['dribble_success'] = e['dribble_outcome'] == 'Complete'
-    e = e.drop(columns=['location', 'pass_end_location', 'carry_end_location'])
-    return e
 
 
 def build_aggregates():
@@ -90,16 +44,29 @@ def build_aggregates():
     return aggregates
 
 
-def do_plots():
-    attribute_chance_plot(build_aggregates(), 'sum_progression_pct', 'var_pass_length', 'chance')
+def do_logistic_regression(x, target='chance'):
+    y = x.pop(target)
+    x = StandardScaler().fit_transform(x)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=0)
 
-    attribute_chance_plot(build_aggregates(), 'sum_progression_pct', 'sum_pass_length', 'chance')
+    model = LogisticRegression()
+    model.fit(x_train, y_train)
 
-    attribute_chance_plot(build_aggregates(), 'sum_progression_pct', 'max_pass_speed', 'chance')
+    model_probs = model.predict_proba(x_test)[:, 1]
+    dumb_probs = [0 for _ in range(len(y_test))]
 
-    attribute_chance_plot(build_aggregates(), 'sum_progression_pct', 'var_pass_speed', 'chance')
+    dumb_auc = roc_auc_score(y_test, dumb_probs)
+    model_auc = roc_auc_score(y_test, model_probs)
 
-    attribute_chance_plot(build_aggregates(), 'location_x', 'location_y', 'chance')
+    print(f'Dumb Constant Guess: ROC AUC={dumb_auc}')
+    print(f'Logistic Regression: ROC AUC={model_auc}')
+    print(model.score(x_test, y_test))
+
+    y_pred = [round(p) for p in model_probs]
+    print(confusion_matrix(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
+
+    return x, y, pd.DataFrame({'actual': y_test, 'pred': y_pred, 'prob': model_probs})
 
 
 def classy():
@@ -108,48 +75,4 @@ def classy():
 
     df = perform_oversampling(aggregates)
 
-    return log_reg(df)
-
-
-def log_reg(x, target='chance'):
-    y = x.pop(target)
-    x = StandardScaler().fit_transform(x)
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=0)
-    model = LogisticRegression()
-    model.fit(x_train, y_train)
-    lr_probs = model.predict_proba(x_test)
-    # keep probabilities for the positive outcome only
-    lr_probs = lr_probs[:, 1]
-    # calculate scores
-    ns_probs = [0 for _ in range(len(y_test))]
-    ns_auc = roc_auc_score(y_test, ns_probs)
-    lr_auc = roc_auc_score(y_test, lr_probs)
-    # summarize scores
-    print('No Skill: ROC AUC=%.3f' % (ns_auc))
-    print('Logistic: ROC AUC=%.3f' % (lr_auc))
-    print(model.score(x_test, y_test))
-    y_pred = [round(p) for p in lr_probs]
-    print(confusion_matrix(y_test, y_pred))
-    return x, y, pd.DataFrame({'actual': y_test, 'pred': y_pred, 'prob': lr_probs})
-
-
-import numpy as np
-from matplotlib import pyplot as plt
-
-
-def attribute_chance_plot(df, x_value, y_value, label_col):
-    df = df[np.abs(df[y_value] - df[y_value].mean()) <= (2 * df[y_value].std())]
-    x = df[x_value].array
-    y = df[y_value].array
-    labels = df[label_col].array
-    cdict = {True: 'red', False: 'blue'}
-
-    fig, ax = plt.subplots()
-    for g in np.unique(labels):
-        ix = np.where(labels == g)
-        ax.scatter(x[ix], y[ix], c=cdict[g], label=g, s=50, marker='2')
-    ax.legend(labels=['No Chance', 'Chance'])
-    plt.title(f'{x_value} vs. {y_value}')
-    plt.xlabel(x_value)
-    plt.ylabel(y_value)
-    plt.show()
+    return do_logistic_regression(df)
--- viz.py
+++ viz.py
@@ -4,14 +4,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 from mplsoccer import Pitch
 
+from lr_agg import build_aggregates
+
 
 def plot_history(history, what):
     x = history.history[what]
     val_x = history.history['val_' + what]
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
     epochs = np.asarray(history.epoch) + 1
-
     plt.plot(epochs, x, 'b', label="Training " + what)
     plt.plot(epochs, val_x, 'g', label="Validation " + what)
     plt.grid()
@@ -39,3 +38,33 @@ def save_embeddings(weights, vocab):
         out_m.write(word + "\n")
     out_v.close()
     out_m.close()
+
+
+def attribute_chance_plot(df, x_value, y_value, label_col):
+    df = df[np.abs(df[y_value] - df[y_value].mean()) <= (2 * df[y_value].std())]
+    x = df[x_value].array
+    y = df[y_value].array
+    labels = df[label_col].array
+    colour_map = {True: 'red', False: 'blue'}
+
+    fig, ax = plt.subplots()
+    for g in np.unique(labels):
+        ix = np.where(labels == g)
+        ax.scatter(x[ix], y[ix], c=colour_map[g], label=g, s=50, marker='2')
+    ax.legend(labels=['No Chance', 'Chance'])
+    plt.title(f'{x_value} vs. {y_value}')
+    plt.xlabel(x_value)
+    plt.ylabel(y_value)
+    plt.show()
+
+
+def do_aggregate_plots():
+    attribute_chance_plot(build_aggregates(), 'sum_progression_pct', 'var_pass_length', 'chance')
+
+    attribute_chance_plot(build_aggregates(), 'sum_progression_pct', 'sum_pass_length', 'chance')
+
+    attribute_chance_plot(build_aggregates(), 'sum_progression_pct', 'max_pass_speed', 'chance')
+
+    attribute_chance_plot(build_aggregates(), 'sum_progression_pct', 'var_pass_speed', 'chance')
+
+    attribute_chance_plot(build_aggregates(), 'location_x', 'location_y', 'chance')
